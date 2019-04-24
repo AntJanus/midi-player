b2f5f1e1cc48ae8fe6a97d878e5d541481787961
antjanus <antonin@antjanus.com>
2019-04-23 20:47:19 -0600

    midi detection message

--- src/MidiInfo.svelte
+++ src/MidiInfo.svelte
@@ -4,9 +4,12 @@
 
 <style>
 </style>
-
-<h1>{primaryInput.name}</h1>
-<h3>
-  {primaryInput.manufacturer}
-</h3>
-<hr />
+{#if !primaryInput.name}
+  No MIDI controller detected :(
+{:else}
+  <h1>{primaryInput.name}</h1>
+  <h3>
+    {primaryInput.manufacturer}
+  </h3>
+  <hr />
+{/if}
